f19088a408fde2001fecdc554e61b4f07887a705
thanut <thanutza_dmex@gmail.com>
2020-03-12 17:48:17 +0700

    Fixed Bey

--- Simple_drawing_window_bey.py
+++ Simple_drawing_window_bey.py
@@ -3,7 +3,7 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 
-class Simple_drawing_window(QWidget):
+class Simple_drawing_window_bey(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
         self.setWindowTitle("Simple Drawing")
@@ -16,7 +16,7 @@ class Simple_drawing_window(QWidget):
         p.setPen(QColor(0,0,0))
         p.setBrush(QColor(0,127,0))
         p.drawPolygon([
-            QPoint(50,200),QPoint(150,200) , QPoint(100,400),
+            QPoint(50,200),QPoint(150,200) , QPoint(100,400), QPoint(200, 400),
         ])
         p.drawPixmap(QRect(200,100,320,320),self.rabbit)
         p.end()
