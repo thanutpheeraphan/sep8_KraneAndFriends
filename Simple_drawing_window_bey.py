d3a10e33ebba9ecf1d4c821599b967bd89af2b70
thanut <thanutza_dmex@gmail.com>
2020-03-12 17:52:42 +0700

    Change Name

--- Simple_drawing_window_bey.py
+++ Simple_drawing_window_bey.py
@@ -3,7 +3,7 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 
-class Simple_drawing_window_bey(QWidget):
+class Simple_drawing_window1(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
         self.setWindowTitle("Simple Drawing")
